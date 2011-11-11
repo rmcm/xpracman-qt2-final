fe342ee37ff6ecadca6ec08b62fdb39b2d0e9162
rmcm <rex@mcmaster.id.au>
2011-11-11 15:21:47 +1100

    Allow editing of GST amount - to fix rounding issues

--- schema/prac/svpf-triggers.sql
+++ schema/prac/svpf-triggers.sql
@@ -1,5 +1,5 @@
 -- -*- sql -*-
- 
+
 --    $Id$
 
 --  Copyright 1996 X=X Computer Software Trust
@@ -10,24 +10,24 @@
 -- it under the terms of the GNU General Public Licensepublished by
 -- the Free Software Foundation; either version 2, or (at your option)
 -- any later version.
- 
+
 -- This software is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
- 
+
 -- You should have received a copy of the GNU General Public License
 -- along with this software; see the file COPYING.  If not, write to
 -- the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
- 
--- Report problems and direct all questions to:
- 
---     Rex McMaster, rmcm@compsoft.com.au
- 
 
- 
+-- Report problems and direct all questions to:
+
+--     Rex McMaster, rmcm@compsoft.com.au
+
+
+
 -- Revision Log
-  
+
 -- ------------------------------------------------------------
 -- PL function to set defaults if null
 -- ------------------------------------------------------------
@@ -36,7 +36,7 @@ create or replace function fn_svpf_null2default()
 returns trigger as $$
 
         DECLARE
-                tmp_invc__sequence svpf.svpf_invc__sequence%TYPE; 
+                tmp_invc__sequence svpf.svpf_invc__sequence%TYPE;
                 tmp_serv_code serv.serv_code%TYPE;
                 tmp_fept fept%ROWTYPE;
                 tmp_patn patn%ROWTYPE;
@@ -114,7 +114,7 @@ returns trigger as $$
             into   tmp_invc
             from   invc
             where  invc__sequence = tmp_invc__sequence;
-            
+
             new.svpf_invc__sequence := tmp_invc.invc__sequence;
           end if;
         end if;
@@ -128,7 +128,7 @@ returns trigger as $$
         and    fept_feet_code = patn_feet_code;
 
         if not found then
-          RAISE EXCEPTION 'item % not found for patient %', new.svpf_serv_code,new.svpf_patn__sequence;
+          RAISE NOTICE 'item % not found for patient %', new.svpf_serv_code,new.svpf_patn__sequence;
           tmp_fept.fept_desc := 'UNKNOWN';
           tmp_fept.fept_amount := 0.00;
           tmp_fept.fept_gst_amount := 0.00;
@@ -162,8 +162,8 @@ returns trigger as $$
            if ( new.svpf_serv_code <> old.svpf_serv_code or new.svpf_percentage <> old.svpf_percentage) then
               new.svpf_amount := fn_round_amount(tmp_fept.fept_amount * (new.svpf_percentage/100::numeric));
            end if;
-           -- always recalculate the GST amount
-           if ( new.svpf_date_service > 'jul 1 2000' ) then
+           -- always recalculate the GST amount if the service amount has changed.
+           if ( new.svpf_date_service > 'jul 1 2000' and old.svpf_amount != new.svpf_amount ) then
              new.svpf_gst_amount := fn_round_amount(new.svpf_amount * (tmp_fept.fept_gst_percentage/100::numeric));
            end if;
            -- final sanity checks
@@ -181,7 +181,7 @@ returns trigger as $$
         end if;
 
         -- And check for %P in desc - replace with provider
-        -- 
+        --
         x_pos := position( '~P' in new.svpf_desc);
         if ( x_pos > 0 ) then
           if ( length(new.svpf_desc) > x_pos + 1 ) then
@@ -218,17 +218,17 @@ returns trigger as $$
     old_invc__sequence := -1;
     new_invc__sequence := -1;
 
-    -- ------------------------------  
+    -- ------------------------------
     -- INSERTS should not be here
-    -- ------------------------------  
+    -- ------------------------------
 
     if (TG_OP != 'UPDATE' and TG_OP != 'DELETE') then
       RAISE EXCEPTION 'tr_svpf_closedinvoice: this trigger can only be used for UPDATE and DELETE';
     end if;
 
-    -- ------------------------------  
+    -- ------------------------------
     -- uninteresting DELETES
-    -- ------------------------------  
+    -- ------------------------------
     if ( TG_OP = 'DELETE' ) then
       if (old.svpf_invc__sequence is null or old.svpf_invc__sequence = 0) then
         return old;
@@ -238,17 +238,17 @@ returns trigger as $$
     end if;
 
 
-    -- ------------------------------  
+    -- ------------------------------
     -- UPDATES may be of interest
-    -- ------------------------------  
+    -- ------------------------------
     if (TG_OP = 'UPDATE') then
       old_invc__sequence := old.svpf_invc__sequence;
       new_invc__sequence := new.svpf_invc__sequence;
     end if;
-    
-    -- ------------------------------  
+
+    -- ------------------------------
     -- look for invc_date_printed
-    -- ------------------------------ 
+    -- ------------------------------
     select invc_date_printed
     into   tmp_invc_date_printed
     from   invc
@@ -256,7 +256,7 @@ returns trigger as $$
            or invc__sequence = new_invc__sequence);
 
     -- ------------------------------
-    -- This trigger might be part 
+    -- This trigger might be part
     -- of an invc delete, so no
     -- parent invc.
     -- ------------------------------
@@ -266,7 +266,7 @@ returns trigger as $$
 
     -- RAISE NOTICE 'tr_svpf_closedinvoice: tmp_invc_date_printed = %, old_invc__sequence = %, new_invc__sequence = %', tmp_invc_date_printed, old_invc__sequence, new_invc__sequence;
 
-    -- ------------------------------  
+    -- ------------------------------
     -- ALREADY PRINTED - only
     -- admin can proceed.
     -- ------------------------------
@@ -284,7 +284,7 @@ returns trigger as $$
     -- Audit deletes after printing.
     -- ------------------------------
     if (TG_OP = 'DELETE' and tmp_invc_date_printed is not null ) then
-  
+
       insert into mtau(mtau_table_name, mtau_row_sequence, mtau_operation, mtau_attributes, mtau_before, mtau_after)
               select    'svpf', old.svpf__sequence, TG_OP,
                         x_sep ||
@@ -315,7 +315,7 @@ returns trigger as $$
     -- ------------------------------
     -- Audit updates after printing
     -- ------------------------------
-  
+
     if ( TG_OP = 'UPDATE' and tmp_invc_date_printed is not null ) then
 
       insert into mtau(mtau_table_name, mtau_row_sequence, mtau_operation, mtau_attributes, mtau_before, mtau_after)
